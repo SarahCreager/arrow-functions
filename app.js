af7fa15a4f753e5c655752a99fa9bc876c6cd513
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-08-11 23:45:07 -0700

    finished question 10

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ console.log('Without parens:', withoutParens('Code 301'));
 // If the code block contains a single line of code, we can write everything on one line
 // We no longer need the curly braces and the return is implicit
 // Without an arrow function, we need to explicitly type "return"
-const oneLiner = course => `I cam currently enrolled in ${course}`;
+const oneLiner = course => `I am currently enrolled in ${course}`;
 
 // DONE: Uncomment the following line of code to see the output in the browser console
 console.log('As a one-liner:', oneLiner('Code 301'));
@@ -108,78 +108,121 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
-  return a + b + c + d;
-};
+// let sum = function(a, b, c, d) {
+//   return a + b + c + d;
+// };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+let sum = (a, b, c, d) => a + b + c + d;
 
-
-let objectLit = function() {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
-
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(sum(1, 2, 3, 4));
 
 
-let sumAndProduct = function(a, b) {
+// let objectLit = function() {
+//   return {
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3',
+//   };
+// };
+
+let objectLit = () => ({
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+});
+
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(objectLit());
+
+
+// let sumAndProduct = function(a, b) {
+//   let sum = a + b;
+//   let product = a * b;
+//   return [sum, product];
+// };
+
+let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
 };
-
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
-
-
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
-
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(sumAndProduct(3, 9));
 
 
-let Student = function(name, age, hometown) {
-  this.name = name;
-  this.age = age;
-  this.hometown = hometown;
-};
+// let message = function(name) {
+//   return `Hello, ${name}!`;
+// };
+
+let message = name => `Hello, ${name}!`;
+
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(message('Allie'));
+
+// ORIGINAL CODE
+// let Student = function(name, age, hometown) {
+//   this.name = name;
+//   this.age = age;
+//   this.hometown = hometown;
+// };
+
+// ARROW FUNCTION
+// let Student = (name, age, hometown) => ({
+//   this.name = name;
+//   this.age = age;
+//   this.hometown = hometown;
+// });
+
+class Student {
+
+  constructor (name, age, hometown) {
+    this.name = name;
+    this.age = age;
+    this.hometown = hometown;
+  }
+
+  greeting () {
+    return `Hi, my name is ${this.name}`;
+  }
+
+}
 
 let joe = new Student('Joe', 'Schmoe', 100);
 
-// TODO: Uncomment the following line of code to see the output in the browser console
+
+// DONE: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
-// TODO: After viewing the previous console.log(), return the code to a working state.
+// DONE: After viewing the previous console.log(), return the code to a working state.
 
+// ORIGINAL CODE
+// Student.prototype.greeting = function() {
+//   return `Hi, my name is ${this.name}`;
+// };
 
+// ARROW FUNCTION
+// Student.prototype.greeting = () => `Hi, my name is ${this.name}`;
 
-Student.prototype.greeting = function() {
-  return `Hi, my name is ${this.name}`;
-};
+// SEE ABOVE ES6 CLASS for prototype greeting.
 
-// TODO: Uncomment the following line of code to see the output in the browser console
+// DONE: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
-// TODO: After viewing the previous console.log(), return the code to a working state.
+// DONE: After viewing the previous console.log(), return the code to a working state.
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+Student.courseName = () => 'This student is enrolled in Code 301.';
+
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(Student.courseName());
 
 
 
